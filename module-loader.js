858f7a8a809af83fc92632c1530d80252942a7da
Ron Apelbaum <ronapelbaum@gmail.com>
2018-03-25 14:42:20 +0300

    change methods

--- module-loader.js
+++ module-loader.js
@@ -19,43 +19,59 @@ window.loadModule = (function (){
         return iframe;
     }
     
-    function createModuleLoadedCllback (mainCallback, onLoadCallback, onIframeWindow) {
-        return function (iframeWindow) {
-            const win = onIframeWindow ? iframeWindow : window;
-            win[mainCallback](iframeWindow.document.body, onLoadCallback);
-        };    
-    }
-    
-    function writeToIframe(iframe, moduleLoadedScript, scriptUrl) {
-        const scriptHtml = scriptUrl ? `<script type="text/javascript" src="${scriptUrl}"></script>` : '';
-        const iframeHtml = `<html><body>${scriptHtml}<script type="text/javascript">${moduleLoadedScript}</script></body></html>`;    
+    // write html to iframe with script
+    function method1 (container, onload, mainCallback, scriptUrl) {
+        const iframe = createIframe();
+        container.appendChild(iframe);
+        iframe.contentWindow['moduleLoaded'] = function (iframeWindow) {
+            iframeWindow[mainCallback](iframe.contentDocument.body, onload);
+        }
+        const moduleLoadedScript = 'moduleLoaded(window)';
+        const iframeHtml = `<html><body><script type="text/javascript" src="${scriptUrl}"></script><script type="text/javascript">${moduleLoadedScript}</script></body></html>`;    
         iframe.contentDocument.open();
         iframe.contentDocument.write(iframeHtml);
         iframe.contentDocument.close();
     }
     
-    function appendToItrame (iframe, moduleLoadedScript) {
+    // append activetion script to iframe
+    function method2 (container, onload, mainCallback) {
+        const iframe = createIframe();
+        container.appendChild(iframe);
+        iframe.contentWindow['moduleLoaded'] = function () {
+            window[mainCallback](iframe.contentDocument.body, onload);
+        }
         const scriptElement = iframe.contentDocument.createElement('script');
-        scriptElement.innerHTML = moduleLoadedScript;
+        scriptElement.innerHTML = 'moduleLoaded()';
         iframe.contentDocument.body.appendChild(scriptElement);
+    }
+    
+    // use iframe onload and use script from parent
+    function method3 (container, onload, mainCallback) {
+        const iframe = createIframe();
+        iframe.onload = function () {
+            window[mainCallback](iframe.contentDocument.body, onload);
+        };
+        container.appendChild(iframe);
     }
     
     return function loadModule(container, options = {}, onload){
         // cleanContainer(container);
         
-        const iframe = createIframe();
-        container.appendChild(iframe);
-        
-        const moduleLoadedName = 'moduleLoded';
-        const moduleLoadedScript = `${moduleLoadedName}(window)`;
-        const moduleLoadedFunction = createModuleLoadedCllback(options.mainCallback, onload, options.mainInIframe);
-        
-        iframe.contentWindow[`${moduleLoadedName}`] = moduleLoadedFunction
-        
-        if (options.writeHtml) {
-            writeToIframe(iframe, moduleLoadedScript, options.scriptUrl);
-        } else {
-            appendToItrame(iframe, moduleLoadedScript)
+        switch (options.method){
+            case 1:
+            method1(container, onload, options.mainCallback, options.scriptUrl);
+            break;
+            
+            case 2:
+            method2(container, onload, options.mainCallback);
+            break;
+            
+            case 3: 
+            method3(container, onload, options.mainCallback);
+            break;
+            
+            default: 
+            // do nothing
         }
     }
 }());
